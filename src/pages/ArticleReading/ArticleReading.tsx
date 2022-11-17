6fd50d4ebd5f437edad418bdc507d41bf899828d
AcediaDotExe <Superbia227@gmail.com>
2022-11-17 12:41:42 +0300

    Add sing in button behavior

--- src/pages/ArticleReading/ArticleReading.tsx
+++ src/pages/ArticleReading/ArticleReading.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Layout from '../../components/layout/Layout/Layout'
+import Layout from '../../components/layout/layout/Layout'
 
 const ArticleReading = () => {
     return (
--- src/pages/Creating/Creating.tsx
+++ src/pages/Creating/Creating.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Layout from '../../components/layout/Layout/Layout'
+import Layout from '../../components/layout/layout/Layout'
 import { Grid } from '@mui/material'
 
 import ArticleEditor from '../../components/editor/articleEditor/ArticleEditor'
