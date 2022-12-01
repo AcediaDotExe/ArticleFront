fcb592df01a7d875bb2ca19703a45709a8daea1f
MediaNik <stafilopok@mail.ru>
2022-12-01 13:47:04 +0300

    Fix authorization url

--- src/assets/authentication/authenticationData.ts
+++ src/assets/authentication/authenticationData.ts
@@ -1,2 +1,2 @@
 export const GoogleSignInLink: string =
-    'http://comgrid.ru:8080/oauth2/authorize/google?redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F'
+    'http://comgrid.ru/article-api/oauth2/authorize/google?redirect_uri={currentHost}'
