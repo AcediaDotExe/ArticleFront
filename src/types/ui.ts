5f83af7efa1645a19aa2682a004b2a0e6e94c494
AcediaDotExe <Superbia227@gmail.com>
2022-12-08 01:26:22 +0300

    Add user avatar to navigation bar and its behavior

--- src/types/ui.ts
+++ src/types/ui.ts
@@ -3,13 +3,15 @@ export enum UiActionType {
     CLOSE_SIDEBAR = 'CLOSE_SIDEBAR',
     OPEN_SIGN_IN_MODAL_WINDOW = 'OPEN_SIGN_IN_MODAL_WINDOW',
     CLOSE_SIGN_IN_MODAL_WINDOW = 'CLOSE_SIGN_IN_MODAL_WINDOW',
+    SET_AVATAR_ANCHOR = 'SET_AVATAR_ANCHOR',
 }
 
 export interface UiState {
     isSidebarOpen: boolean
     isSignInModalOpen: boolean
+    avatarAnchor: null | HTMLElement
 }
 export interface uiAction {
     type: UiActionType
-    payload?: boolean
+    payload?: boolean | HTMLElement
 }
